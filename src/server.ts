962102515fdeb553c8c5d3fe90d1fb44dbe91590
kicheol83 <suyikim7@gmail.com>
2025-06-05 02:14:54 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succed");
+    const PORT = process.env.PORT ?? 3000;
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
