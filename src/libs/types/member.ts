c5cb0e394b13c5d1a97d025abe2bd222bd0a5f1d
kicheol83 <suyikim7@gmail.com>
2025-06-07 03:54:04 +0900

    feat: member ts completed

---
+++ src/libs/types/member.ts
@@ -0,0 +1,34 @@
+import { ObjectId } from "mongoose";
+import { MemberStatus, MemberType } from "../enums/member.enum";
+
+export interface Member {
+  _id: ObjectId;
+  memberType: MemberType;
+  memberStatus: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword?: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberImage?: string;
+  memberPoint: number;
+  createAt: Date;
+  updateAt: Date;
+}
+
+export interface MemberInput {
+  memberType?: MemberType;
+  memberStatus?: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberImage?: string;
+  memberPoint?: number;
+}
+
+export interface LoginInput {
+  memberNick: string;
+  memberPassword: string;
+}
