3acec67818e6531c3e79d78ba34134a9cd231b3c
kicheol83 <suyikim7@gmail.com>
2025-06-12 12:28:56 +0900

    feat: integration makeUploader develop

--- src/libs/utils/uploader.ts
+++ src/libs/utils/uploader.ts
@@ -17,6 +17,7 @@ function getTargetImageStorage(address: any) {
 
 const makeUploader = (address: string) => {
   const storage = getTargetImageStorage(address);
+  console.log(address);
   return multer({ storage: storage });
 };
 
